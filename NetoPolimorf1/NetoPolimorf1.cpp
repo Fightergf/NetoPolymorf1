4e2d88d32bf50663ba9bb029e396755cb8591e5c
DenisMir <pete4kob12@mail.ru>
2022-09-01 21:54:16 +0300

    work1

---
+++ NetoPolimorf1/NetoPolimorf1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class Figure
+{
+    private:
+    int side = 0;
+public:
+    int GetSide()
+    {
+        return side;
+    }
+    int SetSide(int side)
+    {
+        return this->side = side;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+    int sides = SetSide(3);
+    void printF()
+    {
+            std::cout << "Треугольник: " << sides << "\n";
+    }
+};
+
+class Quadrilateral : public Figure
+{
+public:
+    int sides = SetSide(4);
+    void printF()
+    {
+        std::cout << "Квадрат: " << sides << "\n";
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    Figure FIG;
+    Triangle TRI;
+    Quadrilateral QUAD;
+    std::cout << "Колличество сторон: \n";
+    std::cout << "Фигура: " << FIG.GetSide() << "\n";
+    TRI.printF();
+    QUAD.printF();
+}
